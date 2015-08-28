6a6e2268f87088fb7c5cc38937b8111666adf7f5
Sam Hampton <sam.hampton@advancedcomputersoftware.com>
2015-08-28 13:35:52 +0100

    Read from and update Firebase database from front end.

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-var app = angular.module('todo', ['ionic', 'firebase']);
-
-app.controller('TodoCtrl', function($scope, $firebaseArray) {
-  var ref = new Firebase("https://luminous-torch-9880.firebaseio.com/categories");
-  
-  
-  // download the data into a local object
-  $scope.categories = $firebaseArray(ref);
-  
- // syncObject.$bindTo($scope, "data");
-  
-  
-})
+var app = angular.module('budget', ['ionic']);
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -29,3 +12,4 @@ app.run(function($ionicPlatform) {
     }
   });
 })
+
